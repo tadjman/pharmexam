9f7c3aa8f406991a97a7c978d6f025313949df01
Adam TADJINE <t.adam9206@outlook.fr>
2026-02-18 16:26:57 +0100

    feat: ajout des apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts",
+    "academics",
+    "exams",
+    "rooms",
+    "assignments",
+    "reports",
 ]
 
 MIDDLEWARE = [
